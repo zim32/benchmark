ea81e72b0e79c21fbcab212bc52853fa35962e4e
zim32 <yurij.uvarov@gmail.com>
2023-11-27 12:18:31 +0100

    Part 5

---
+++ java-quarkus/code-with-quarkus/src/main/java/org/acme/model/Circle.java
@@ -0,0 +1,24 @@
+package org.acme.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Circle implements FigureInterface{
+    private final int diameter;
+
+    @JsonCreator
+    Circle(@JsonProperty("diameter") int diameter) {
+        this.diameter = diameter;
+    }
+
+    @Override
+    public String getType() {
+        return "circle";
+    }
+
+    @Override
+    public double computeArea() {
+        var r = (double)diameter / 2;
+        return Math.PI * Math.pow(r, 2);
+    }
+}
---
+++ java-quarkus/code-with-quarkus/src/main/java/org/acme/model/FigureInterface.java
@@ -0,0 +1,20 @@
+package org.acme.model;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "type"
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = Square.class, name = "square"),
+        @JsonSubTypes.Type(value = Rectangle.class, name = "rectangle"),
+        @JsonSubTypes.Type(value = Triangle.class, name = "triangle"),
+        @JsonSubTypes.Type(value = Circle.class, name = "circle"),
+})
+public interface FigureInterface {
+    public String getType();
+    public double computeArea();
+}
